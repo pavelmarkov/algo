8e601f67e324f0c1dd7bbd5bb9b0f8f5e87600f5
Марков Павел Леонидович <pl_markov@controlsystems.ru>
2024-05-29 08:34:03 +0500

    print tree

--- algorithms/binary-tree.py
+++ algorithms/binary-tree.py
@@ -1,3 +1,5 @@
+# python3 ./algorithms/binary-tree.py
+
 class Node:
   def __init__(self, value = None):
     self.value = value
@@ -47,28 +49,11 @@ class BinaryTree:
   
 
   
-  def print(self):
-    print('=========')
-    node = self.root
-    queue = [ node ]
-    print(node.value, '\n')
-    while(len(queue) > 0):
-      node = queue.pop(0)
-      if(node):
-        trio = []
-        trio.append(node.value)
-        if(node.left):
-          queue.append(node.left)
-          trio.append(node.left.value)
-        else:
-          trio.append(None)
-        if(node.right):
-          queue.append(node.right)
-          trio.append(node.right.value)
-        else:
-          trio.append(None)
-        print(trio)
-    print('=========')
+  def print(self, startingNodes = None):
+    print('tree: ')
+    numLevels = self.printPiramid(startingNodes)
+    print('-=-' * (numLevels - 2))
+    print('\n')
 
   def countChildren(self, node):
     count = 0
@@ -79,11 +64,8 @@ class BinaryTree:
     return count
 
   def delete(self, value):
-    print(f'deleting node with value {value}')
     deleteNode = self.findNodeByValue(value)
-    print(deleteNode.value)
     childNum = self.countChildren(deleteNode)
-    print(childNum)
     if(childNum == 0):
       deleteNode = None
     elif(childNum == 1):
@@ -99,7 +81,33 @@ class BinaryTree:
       else:
         deleteNode.value = deleteNode.right.value
         deleteNode.right = deleteNode.right.right
-        print(deleteNode.value, deleteNode.left.value, deleteNode.right )
+  
+  def printPiramid(self, nodes = None, level = 0):
+    if(not nodes):
+      maxElements = self.printPiramid([ self.root ], 0)
+      print(' ' * (maxElements - 1) + str(self.root.value))
+      return maxElements
+    nodeExist = False
+    for node in nodes:
+      if(node):
+        nodeExist = True
+        break
+    numOfNodesOnThisLevel = pow(2, level)
+    if(not nodeExist):
+      return numOfNodesOnThisLevel
+    thisLevelNodes = []
+    for node in nodes:
+      if(not node):
+        thisLevelNodes.append(None)
+        thisLevelNodes.append(None)
+        continue
+      thisLevelNodes.append(node.left)
+      thisLevelNodes.append(node.right)
+    maxNum = self.printPiramid(thisLevelNodes, level+1)
+    treeRow = [((node and str(node.value)) or ' ') for node in thisLevelNodes]
+    padding = ' ' * (maxNum // len(treeRow))
+    print(padding + padding.join(treeRow) + padding)
+    return maxNum
 
 
 binaryTree = BinaryTree(20)
@@ -110,18 +118,26 @@ binaryTree.insert(13)
 binaryTree.insert(27)
 binaryTree.insert(33)
 
+
+print('printPiramid(): ', '\n')
 binaryTree.print()
 
-findNode = binaryTree.findNodeByValue(30);
-print(findNode.left.value)
+findNode = binaryTree.findNodeByValue(30)
+print('findNodeByValue(30): ', findNode.left.value, '\n')
 
 minNode, minNodeParent = binaryTree.findMinNodeInSubtree(findNode)
-print(minNode.value)
+print('binaryTree.findMinNodeInSubtree(findNode): ', minNode.value, '\n')
 
 binaryTree.delete(10)
+print('delete(10): ', '\n')
 binaryTree.print()
 
+binaryTree.insert(40)
+binaryTree.insert(15)
+binaryTree.insert(2)
+binaryTree.insert(25)
+binaryTree.insert(29)
+binaryTree.insert(32)
+binaryTree.print()
+binaryTree.print([binaryTree.root.right])
 
-
-# binaryTree.insert(15)
-# binaryTree.print()
